220c0742f98f5c208b4dfab27801c1a1ad6e501d
yvartpro <yvart.pro@gmail.com>
2025-04-08 15:16:22 +0200

    background image

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
- navigator.serviceWorker.register('sw.js').then(registration => console.log('Service worker registered: ', registration))
+ navigator.serviceWorker.register('service-worker.js').then(registration => console.log('Service worker registered: ', registration))
 }else{
     console.log(false)
 }
