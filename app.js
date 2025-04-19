c73aa22cc6f13d47597a7fd5768b503c5c4adfd4
yvartpro <yvart.pro@gmail.com>
2025-04-19 23:40:26 +0000

    Start notificarion system

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+//register SW
 if('serviceWorker' in navigator){
  window.onload = () => navigator.serviceWorker.register('./service-worker.js').catch(err => console.error(err))
 }
+
+//request notificationn
+if("Notification" in window) {
+  if(Notification.permission === "granted"){
+    new Notification("Giraso",{
+      body: "You have a new message",
+      icon: "icon.png"
+    })
+  }else{
+    Notification.requestPermission().then(perm =>{
+      if(perm === "granted") {
+        new Notification("Giraso",{
+        body: "Activation fast",
+        icon: "icon.png"
+    })
+  }
+
