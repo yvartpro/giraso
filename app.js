a3f65ae1f43608a2b27ac36421e14332547229c4
yvartpro <yvart.pro@gmail.com>
2025-04-20 05:31:44 +0000

    Notification

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ if('serviceWorker' in navigator){
  window.onload = () => navigator.serviceWorker.register('./service-worker.js').catch(err => console.error(err))
 }
 
-//request notificationn
-if("Notification" in window) {
-  if(Notification.permission === "granted"){
-    new Notification("Giraso",{
-      body: "You have a new message",
-      icon: "icon.png"
-    })
-  }else{
-    Notification.requestPermission().then(perm =>{
-      if(perm === "granted") {
-        new Notification("Giraso",{
-          body: "Activation fast",
-          icon: "icon.png"
-        })
-      }
-    })
+//notify user
+const notifyUser = () => { 
+ if("Notification" in window) {
+   if(Notification.permission === "granted"){
+      new Notification("Giraso",{
+        body: "You have a new message",
+        icon: "icon.png"
+      })
+    }else{
+      Notification.requestPermission().
+      then(perm =>{
+        if(perm === "granted") {
+          new Notification("Giraso",{
+            body: "Activation fast",
+            icon: "icon.png"
+          })
+        }
+      })
+    }
   }
+  console.log(true)
 }
-console.log(true)
